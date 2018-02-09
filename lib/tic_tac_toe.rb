009b18f7f2cbd9d1b1296cf3a832a26a6f509f9a
Laura Messa Travin <laura.travin@gmail.com>
2018-02-09 19:57:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,11 +23,11 @@ def display_board
 end
 
 def input_to_index(user_input)
-   @index = user_input.to_i - 1
+   index = user_input.to_i - 1
 end
 
-def move(value)
-  @board[@index] = value
+def move(index,value = "X")
+  @board[index] = value
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
