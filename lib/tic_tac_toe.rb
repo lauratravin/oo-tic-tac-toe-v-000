529770f366c94fc21a9f97be06575a482fe1ee2a
Laura Messa Travin <laura.travin@gmail.com>
2018-02-09 20:48:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,11 +28,12 @@ end
 
 def move(index,value)
   @board[index] = value
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-  puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-  puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  # puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  # puts "-----------"
+  # puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  # puts "-----------"
+  # puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  display_board
 end
 
 def position_taken?(index)
