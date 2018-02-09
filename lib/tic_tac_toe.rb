4c19da109591464214cb771e95430e05d4c178af
Laura Messa Travin <laura.travin@gmail.com>
2018-02-09 19:27:29 +0000

    automatically backed up by learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,17 @@
+class TicTacToe
+#  def initialize(board = nil)
+#    @board = board 
+#   
+# end  
+
+
+
+def initialize(board = nil)
+  if board == nil
+    @board = Array.new(9," ")
+  else
+    @board = board
+  end
+
+
+end  
